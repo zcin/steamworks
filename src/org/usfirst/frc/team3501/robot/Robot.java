a3de0f87a053c1a06ed966f6eb3744936373eb21
Cindy Zhang <cindyzyx9@gmail.com>
2017-01-19 19:30:18 -0800

    add getter methods for subsystems

--- src/org/usfirst/frc/team3501/robot/Robot.java
+++ src/org/usfirst/frc/team3501/robot/Robot.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team3501.robot;
 
+import org.usfirst.frc.team3501.robot.subsystems.Climber;
 import org.usfirst.frc.team3501.robot.subsystems.DriveTrain;
+import org.usfirst.frc.team3501.robot.subsystems.Intake;
+import org.usfirst.frc.team3501.robot.subsystems.Shooter;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -17,6 +20,18 @@ public class Robot extends IterativeRobot {
 
   public static DriveTrain getDriveTrain() {
     return DriveTrain.getDriveTrain();
+  }
+
+  public static Climber getClimber() {
+    return Climber.getClimber();
+  }
+
+  public static Intake getIntake() {
+    return Intake.getIntake();
+  }
+
+  public static Shooter getShooter() {
+    return Shooter.getShooter();
   }
 
   public static OI getOI() {
